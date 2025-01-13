5ecec5136d14e5482de4fa5d9b97133690066228
Jaeyun Lee <jleegidon@gmail.com>
2025-01-13 23:30:15 +0900

    initial commit

---
+++ pytorch/test.py
@@ -0,0 +1,12 @@
+"""
+'''
+"""
+
+def test_function(text: str) -> None:
+    """
+    test function
+     - param text: string text to print out
+    """
+    print(text)
+
+test_function(text="Hello World")
